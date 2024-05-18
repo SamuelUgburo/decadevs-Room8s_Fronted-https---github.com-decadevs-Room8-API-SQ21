a2753f453bb4e046536992052192396246093827
Kehinde Olubiyi <kehinde.olubiyi@decagon.dev>
2024-05-18 22:10:46 +0100

    Signup frontEnd page

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
 import LoginPage from "./pages/LoginPage"
+import SignupPage from "./pages/SignupPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />} >
       <Route index element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage/>}/>
     </Route>
   )
 );

d13623254851e437f653aeb49b3baef09d1aa742
dorisjenny27 <dorisjenny27@gmail.com>
2024-05-18 19:04:34 +0100

    Register Number Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
 import ConfirmPassword from "./pages/ConfirmPassword";
-import LoginPage from "./pages/LoginPage"
+import LoginPage from "./pages/LoginPage";
+import RegisterNumber from "./pages/RegisterNumber";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       <Route path="/confirmpassword" element={<ConfirmPassword />} />
       <Route index element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/registernumber" element={<RegisterNumber/>} />
     </Route>
   )
 );
